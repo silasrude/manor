e504ece82426d3a8099ff7699788b97e6211c711
silasrude <69167335+silasrude@users.noreply.github.com>
2024-11-05 16:40:11 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@ var userinput = prompt("Enter your username.")
 if (userinput == "billmurphy") {
     console.log("valid user name");
 } else  {
-    window.location.href = "/home/silasrude/mdn/three/index.html";
+    window.location.href = "https://silasrude.github.io/manor/";
 }
 
