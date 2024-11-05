e7b2a9c968623bba152df1973e2768eccb851295
silasrude <69167335+silasrude@users.noreply.github.com>
2024-11-05 16:13:36 -0600

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var userinput = prompt("Enter your username.")
-
-if (userinput == "billmurphy") {
-    console.log("valid user name");
-} else  {
-    window.location.href = "/home/silasrude/mdn/three/index.html";
-}
-
