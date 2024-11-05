4809c8b83f90d4e5e9dbfe44b779854b0e8344e2
silasrude <69167335+silasrude@users.noreply.github.com>
2024-11-05 15:53:54 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var userinput = prompt("Enter your username.")
+
+if (userinput == "billmurphy") {
+    console.log("valid user name");
+} else  {
+    window.location.href = "/home/silasrude/mdn/three/index.html";
+}
+
